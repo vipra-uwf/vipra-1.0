e9534474be8f8b90bebb7e4ab467061cde49ccad
LightningToaster <lightningtoaster@gmail.com>
2020-05-29 13:13:29 -0500

    comments

--- VIPRA/pedestrian_set.hpp
+++ VIPRA/pedestrian_set.hpp
@@ -8,19 +8,19 @@ class PedestrianSet{
         int getNumPedestrians();
         std::vector<FLOATING_NUMBER>* getXCoordinates();
         std::vector<FLOATING_NUMBER>* getYCoordinates();
-        /*std::vector<FLOATING_NUMBER>* getDesiredVelocities();
+        std::vector<FLOATING_NUMBER>* getDesiredVelocities();
         std::vector<FLOATING_NUMBER>* getXVelocityDirection();
         std::vector<FLOATING_NUMBER>* getYVelocityDirection();
         std::vector<FLOATING_NUMBER>* getMasses_kg();
-        std::vector<FLOATING_NUMBER>* getPropulsionForces();*/
+        std::vector<FLOATING_NUMBER>* getPropulsionForces();
 
     private:
         int numPedestrians;
         std::vector<FLOATING_NUMBER> xCoordinates;
         std::vector<FLOATING_NUMBER> yCoordinates;
-        /*std::vector<FLOATING_NUMBER> desiredVelocities;
+        std::vector<FLOATING_NUMBER> desiredVelocities;
         std::vector<FLOATING_NUMBER> xVelocityDirection;
         std::vector<FLOATING_NUMBER> yVelocityDirection;
         std::vector<FLOATING_NUMBER> masses_kg;
-        std::vector<FLOATING_NUMBER> propulsionForces;*/
+        std::vector<FLOATING_NUMBER> propulsionForces;
 };
