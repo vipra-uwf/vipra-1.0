346c1e1cd11501b9df2920a7674333cc93c09c60
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-06-09 10:52:22 -0500

    added methods that OutputDataWriter will need

--- VIPRA/output_data_writer.hpp
+++ VIPRA/output_data_writer.hpp
@@ -1,12 +1,17 @@
 #ifndef OUTPUT_DATA_WRITER_HPP 
 #define OUTPUT_DATA_WRITER_HPP 
 
+#include <string>
+
 #include "data.hpp"
+#include "type_definitions.hpp"
 
 class OutputDataWriter
 {
     public:
         virtual void writeData(Data* data) = 0;
+        virtual void writeFloatData(std::string, FLOATING_NUMBER) = 0;
+        virtual void writeStringData(std::string, std::string) = 0;
 };
 
 #endif
