97068c8028b466547d37775119256427f7a62d39
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-07-04 21:50:44 -0500

    can now simulate a change in plot over time

--- UtilityScripts/coordinates_to_png/main.cpp
+++ UtilityScripts/coordinates_to_png/main.cpp
@@ -12,18 +12,34 @@ int main() {
     std::vector<FLOATING_NUMBER> pedestrianX = xmlReader.getFloatDataSet("x");
     std::vector<FLOATING_NUMBER> pedestrianY = xmlReader.getFloatDataSet("y");
 
-
     xmlReader.extractFileData("a320_144_obstacles.xml", "obstacle-set", "obstacle");
     std::vector<FLOATING_NUMBER> obstacleX = xmlReader.getFloatDataSet("x");
     std::vector<FLOATING_NUMBER> obstacleY = xmlReader.getFloatDataSet("y");
 
+    int i = 0;
 
-    plt::xlim(-30, 30);
-    plt::ylim(-30, 30); 
-	plt::plot(pedestrianX, pedestrianY, "r.");
-    plt::scatter(obstacleX, obstacleY, 1);
-    plt::save("obstacles.png");
-    plt::show();
+    while(i < pedestrianX.size())
+    {
+        pedestrianX.at(i) = pedestrianX.at(i) * 1.1;
+        pedestrianY.at(i) = pedestrianY.at(i) * 1.1;
+
+        plt::clf();
+
+        plt::xlim(-30, 30);
+        plt::ylim(-30, 30); 
+        plt::plot(pedestrianX, pedestrianY, "r.");
+        plt::scatter(obstacleX, obstacleY, 1);
+
+        plt::pause(0.01);
+        i++;
+    }
+
+    // plt::xlim(-30, 30);
+    // plt::ylim(-30, 30); 
+    // plt::plot(pedestrianX, pedestrianY, "r.");
+    // plt::scatter(obstacleX, obstacleY, 1);
+    // plt::save("obstacles.png");
+    // plt::show();
 }
 
 // g++ main.cpp xml_reader.cpp -I/usr/include/python3.8 -lpython3.8
