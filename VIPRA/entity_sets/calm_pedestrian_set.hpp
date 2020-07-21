98489bd956de58838cc137db7d5875097162e76d
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-07-21 13:35:51 -0500

    fixed 2 space tabs problem in CalmPedestrianSet

--- VIPRA/entity_sets/calm_pedestrian_set.hpp
+++ VIPRA/entity_sets/calm_pedestrian_set.hpp
@@ -7,77 +7,77 @@
 
 class CalmPedestrianSet: public PedestrianSet
 {
-  private:
-    int numPedestrians;
+    private:
+        int numPedestrians;
 
-    //TODO this should be const then 
-    //delete numPed setter and getter - alex
-    int numDimensions = 2;
+        //TODO this should be const then 
+        //delete numPed setter and getter - alex
+        int numDimensions = 2;
 
-    std::vector<Dimensions> pedestrianCoordinates;
-    std::vector<Dimensions> goalCoordinates;
-    std::vector<Dimensions> velocities;
+        std::vector<Dimensions> pedestrianCoordinates;
+        std::vector<Dimensions> goalCoordinates;
+        std::vector<Dimensions> velocities;
 
-    std::vector<FLOATING_NUMBER> speedsMetersPerSecond;
-    std::vector<FLOATING_NUMBER> massesKg;
-    std::vector<FLOATING_NUMBER> reactionTimes;
-    std::vector<FLOATING_NUMBER> desiredSpeeds;
-    std::vector<FLOATING_NUMBER> propulsionForces;
-    std::vector<FLOATING_NUMBER> repulsionForces;
-    std::vector<int> nearestNeighbors;
+        std::vector<FLOATING_NUMBER> speedsMetersPerSecond;
+        std::vector<FLOATING_NUMBER> massesKg;
+        std::vector<FLOATING_NUMBER> reactionTimes;
+        std::vector<FLOATING_NUMBER> desiredSpeeds;
+        std::vector<FLOATING_NUMBER> propulsionForces;
+        std::vector<FLOATING_NUMBER> repulsionForces;
+        std::vector<int> nearestNeighbors;
 
-    void initializeSpeeds();
-    void initializeGoals();
-    void initializeVelocities();
-    void initializePropulsionForces();
-    void initializeRepulsionForces();
-    void initializeNearestNeighbors();
+        void initializeSpeeds();
+        void initializeGoals();
+        void initializeVelocities();
+        void initializePropulsionForces();
+        void initializeRepulsionForces();
+        void initializeNearestNeighbors();
 
 
-  public:
-    CalmPedestrianSet();
+    public:
+        CalmPedestrianSet();
 
-    virtual void initializeValues();
+        virtual void initializeValues();
 
-    virtual int getNumPedestrians();
-    virtual int getNumDimensions();
-    virtual std::vector<Dimensions>* getPedestrianCoordinates();
-    virtual std::vector<Dimensions>* getGoalCoordinates();
-    virtual std::vector<Dimensions>* getVelocities();
-    virtual std::vector<FLOATING_NUMBER>* getSpeeds();
+        virtual int getNumPedestrians();
+        virtual int getNumDimensions();
+        virtual std::vector<Dimensions>* getPedestrianCoordinates();
+        virtual std::vector<Dimensions>* getGoalCoordinates();
+        virtual std::vector<Dimensions>* getVelocities();
+        virtual std::vector<FLOATING_NUMBER>* getSpeeds();
 
-    // TODO these should not be virtual - alex
-    virtual std::vector<FLOATING_NUMBER>* getMasses();
-    virtual std::vector<FLOATING_NUMBER>* getReactionTimes();
-    virtual std::vector<FLOATING_NUMBER>* getDesiredSpeeds();
-    virtual std::vector<FLOATING_NUMBER>* getPropulsionForces();
-    virtual std::vector<FLOATING_NUMBER>* getRepulsionForces();
-    virtual std::vector<int>* getNearestNeighbors();
+        // TODO these should not be virtual - alex
+        virtual std::vector<FLOATING_NUMBER>* getMasses();
+        virtual std::vector<FLOATING_NUMBER>* getReactionTimes();
+        virtual std::vector<FLOATING_NUMBER>* getDesiredSpeeds();
+        virtual std::vector<FLOATING_NUMBER>* getPropulsionForces();
+        virtual std::vector<FLOATING_NUMBER>* getRepulsionForces();
+        virtual std::vector<int>* getNearestNeighbors();
 
-    virtual void setNumPedestrians(int numPedestrians);
-    virtual void setNumDimensions(int numDimensions);
-    virtual void setPedestrianCoordinates(
-    std::vector<Dimensions> coordinates);
-    virtual void setGoalCoordinates(
-    std::vector<Dimensions> goalCoordinates);
-    virtual void setVelocities(
-    std::vector<Dimensions> velocities);
-    virtual void setSpeeds(
-    std::vector<FLOATING_NUMBER> speedsMetersPerSecond);
+        virtual void setNumPedestrians(int numPedestrians);
+        virtual void setNumDimensions(int numDimensions);
+        virtual void setPedestrianCoordinates(
+        std::vector<Dimensions> coordinates);
+        virtual void setGoalCoordinates(
+        std::vector<Dimensions> goalCoordinates);
+        virtual void setVelocities(
+        std::vector<Dimensions> velocities);
+        virtual void setSpeeds(
+        std::vector<FLOATING_NUMBER> speedsMetersPerSecond);
 
-    //TODO these shouldnt be virtual - alex
-    virtual void setMasses(std::vector<FLOATING_NUMBER> massesKg);
-    virtual void setReactionTimes(
-    std::vector<FLOATING_NUMBER> reactionTimes);
-    virtual void setDesiredSpeeds(
-    std::vector<FLOATING_NUMBER> desiredSpeeds);
-    virtual void setPropulsionForces(
-    std::vector<FLOATING_NUMBER> propulsionForces);
-    virtual void setRepulsionForces(
-    std::vector<FLOATING_NUMBER> repulsionForces);
+        //TODO these shouldnt be virtual - alex
+        virtual void setMasses(std::vector<FLOATING_NUMBER> massesKg);
+        virtual void setReactionTimes(
+        std::vector<FLOATING_NUMBER> reactionTimes);
+        virtual void setDesiredSpeeds(
+        std::vector<FLOATING_NUMBER> desiredSpeeds);
+        virtual void setPropulsionForces(
+        std::vector<FLOATING_NUMBER> propulsionForces);
+        virtual void setRepulsionForces(
+        std::vector<FLOATING_NUMBER> repulsionForces);
 
-    //TODO should probably make this plural - alex	
-    virtual void setNearestNeighbors(std::vector<int> nearestNeighbors);
+        //TODO should probably make this plural - alex	
+        virtual void setNearestNeighbors(std::vector<int> nearestNeighbors);
 };
 
 #endif
