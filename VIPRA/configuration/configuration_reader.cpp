047282d05abe3817b9f27dba651faf546d47d343
Liam Hilton-Green <liamhg1@yahoo.com>
2020-12-03 15:50:20 -0600

    Created configuration_reader.

---
+++ VIPRA/configuration/configuration_reader.cpp
@@ -0,0 +1,25 @@
+#include "configuration_reader.hpp"
+
+void ConfigurationReader::readJsonConfiguration(std::string fileName)
+{
+    openFile(fileName);
+    parseFile();
+}
+
+Json::Value ConfigurationReader::getJsonObject()
+{
+    return this->jsonDocument;
+}
+
+void ConfigurationReader::openFile(std::string fileName)
+{
+    this->fileStream.open(fileName);
+}
+
+void ConfigurationReader::parseFile()
+{
+    if(!Json::parseFromStream(this->jsonReader, this->fileStream, &this->jsonDocument, &this->errors))
+    {
+		std::cout << "Error parsing JSON: " << errors << std::endl;
+    }
+}
