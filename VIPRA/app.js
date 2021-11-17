a8b35c2febb492aabc5f135368e87fe252b060d0
root <root@ubuntu1804.localdomain>
2021-11-17 02:25:21 +0000

    Renamed index.js -> app.js. Added controller for main directory of webpage

--- VIPRA/app.js
+++ VIPRA/app.js
@@ -1,19 +1,16 @@
 const port = 3000;
 const express = require("express");
 const exec = require("child_process").exec;
-
 const app = express();
 
 let vipraProcess = exec('./run.sh');
 vipraProcess.stdout.on('data', function(data) {
-	console.log(data);
+		console.log(data);
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello\n");
-})
+let main = require('./controllers/main');
+
+app.get("/", main.home);
 
 app.listen(port);
 console.log(`The server has started and is listening on port number:${port}`);
-
-
