dae23ac104f8ab57f1d2dd96243f9c48fe46cf69
GoodenoughR <rtg13@students.uwf.edu>
2022-02-15 18:37:18 -0600

    fixed run not working

--- VIPRA/webAPI/routes/API_routes.js
+++ VIPRA/webAPI/routes/API_routes.js
@@ -6,29 +6,6 @@ const bodyParser = require('body-parser')
 const router = express.Router();
 const exec = require("child_process").exec;
 
-
-
-// TODO Might try to send output data even if the simulation errors out
-const simulation = "./test";
-const runSim = (res)=>{
-    exec(simulation, (err, stdout, stderr)=>{
-        if(err){
-            console.log(err);
-        }
-        if(stdout){
-            console.log(stdout);
-        }
-        if(stderr){
-            console.log(stderr);
-        }
-    }).on('close', ()=>{
-        console.log("Done");
-        res.send("Done");
-    });
-}
-
-
-
 // TODO may not be async
 const sim_options = fs.readFileSync(path.resolve(__dirname, './../../input_data/sim_options.json'));
 const input_data_options = path.resolve(__dirname, './../../input_data/sim_config.json');
@@ -42,6 +19,27 @@ router.get("/api", (req, res) =>{
 	// send sim_options.json
 	res.send(JSON.parse(sim_options));
 });
+
+
+// TODO Might try to send output data even if the simulation errors out
+const simulation = path.resolve(__dirname, './../run');
+const runSim = (res)=>{
+    exec(simulation, (err, stdout, stderr)=>{
+        if(err){
+            console.log(err);
+        }
+        if(stdout){
+            console.log(stdout);
+        }
+        if(stderr){
+            console.log(stderr);
+        }
+    }).on('close', ()=>{
+        const output = fs.readFileSync(output_data_path);
+        res.send(output);
+    });
+}
+
 
 // TODO run the json through a middleware to make sure its correct
 router.post("/api", (req, res) =>{
