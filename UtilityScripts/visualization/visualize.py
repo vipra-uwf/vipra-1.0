a5459ce120ea1e4d6d3b5a28c3cf68b17b26bcd8
GoodenoughR <rtg13@students.uwf.edu>
2022-07-25 19:04:52 -0500

    added obstacles to visualizer

---
+++ UtilityScripts/visualization/visualize.py
@@ -0,0 +1,42 @@
+import json
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+
+if(len(sys.argv) != 3):
+    print("USAGE: python3 visualizer.py *pedestrian_trajectory.json* *obstacle_set.json*")
+    exit(-1)
+
+
+peds = open(sys.argv[1])
+obs = open(sys.argv[2])
+
+data = json.load(peds)
+obstacles = json.load(obs)
+
+obsX = []
+obsY = []
+
+for obj in obstacles["obstacles"]:
+    obsX.append(float(obj["x"]))
+    obsY.append(float(obj["y"]))
+
+
+ax = plt.gca()
+
+for step in data:
+   plt.pause(0.05)
+   plt.cla()
+   ax.set_ylim(-5, 20)
+   ax.set_xlim(-5, 30)
+   ax.autoscale(False)
+   plt.title(step)
+   pointsX = []
+   pointsY = []
+   for key in data[step]:
+       pointsX.append(key["x"])
+       pointsY.append(key["y"])
+   plt.scatter(obsX, obsY, 1)
+   plt.scatter(pointsX, pointsY, 2)
+
+plt.show()
