94ea8f7d4eafaf00f4f7abd4f49cafb006251038
Nathan Rayon <nrayon@DESKTOP-JCMRR7J.localdomain>
2022-10-12 12:46:26 -0500

    Added pedestrian set and simple test for initializing pedestrian set

--- Modules/PedestrianSet/calm_pedestrian_set/calm_pedestrian_set.hpp
+++ Modules/PedestrianSet/calm_pedestrian_set/calm_pedestrian_set.hpp
@@ -5,19 +5,15 @@
 #include <string>
 #include <utility>
 
-#include "../../VIPRA_DEV/Base/definitions/race_status.hpp"
-#include "../../VIPRA_DEV/Extendable/pedestrianset/pedestrian_set.hpp"
-#include "../../definitions/type_definitions.hpp"
+#include "../pedestrian_set.hpp"
 
 class CalmPedestrianSet : public PedestrianSet {
  public:
+        CalmPedestrianSet();
+
         void configure(const CONFIG_MAP& configMap) override;
         void initialize(size_t pedestrianCount) override;
-
-        void setCoordinates(DimVector&&) override;
-
-        const Dimensions& getPedestrianCoords(size_t index) const override;
-        const DimVector&  getAllPedCoords() const override;
+        void removePedestrian(size_t pedestrianIndex) override;
 
 
         [[nodiscard]] int                                       getNumPedestrians()                                     const noexcept override;
@@ -25,6 +21,7 @@ class CalmPedestrianSet : public PedestrianSet {
 
         [[nodiscard]] const DimVector&            getPedestrianCoordinates()                              const noexcept override;
         [[nodiscard]] const DimVector&            getVelocities()                                         const noexcept override;
+        [[nodiscard]] const DimVector&            getGoalCoordinates();
         [[nodiscard]] const std::vector<FLOATING_NUMBER>&       getSpeeds()                                             const noexcept override;
 
         void                                    setNumPedestrians(int numPedestrians)                                           override;
@@ -39,52 +36,25 @@ class CalmPedestrianSet : public PedestrianSet {
         void                                    setVelocity(Dimensions&& velocity, size_t index)                                override;
         void                                    setVelocities(const DimVector& velocities)                        noexcept override;
         void                                    setVelocities(DimVector&& velocities)                             noexcept override;
-
+        
         void                                    setSpeed(FLOATING_NUMBER speed, size_t index)                                   override; 
         void                                    setSpeeds(const std::vector<FLOATING_NUMBER>& speedsMetersPerSecond)            noexcept override;
         void                                    setSpeeds(std::vector<FLOATING_NUMBER>&& speedsMetersPerSecond)                 noexcept override;
 
-        // virtual ~CalmPedestrianSet();
-
         [[nodiscard]] const std::vector<FLOATING_NUMBER>&                     getMasses()                     const noexcept;
         [[nodiscard]] const std::vector<FLOATING_NUMBER>&                     getReactionTimes()              const noexcept;
         [[nodiscard]] const std::vector<FLOATING_NUMBER>&                     getDesiredSpeeds()              const noexcept;
-        [[nodiscard]] const std::vector<FLOATING_NUMBER>&                     getRepulsionForces()            const noexcept;
-        [[nodiscard]] const std::vector<FLOATING_NUMBER>&                     getPriorities()                 const noexcept;
         [[nodiscard]] const std::vector<FLOATING_NUMBER>&                     getShoulderLengths()            const noexcept;
 
         
         [[nodiscard]] const std::vector<int>&                                 getStartingAisles()             const noexcept;
-        [[nodiscard]] const std::vector<int>&                                 getRaceCounter()                const noexcept;
-        [[nodiscard]] const std::vector<int>&                                 getOpponentIDs()                const noexcept;
-        
-        [[nodiscard]] const DimVector&                                        getRacePositions()              const noexcept;
-        [[nodiscard]] const DimVector&                                        getPropulsionForces()           const noexcept;
-
-        [[nodiscard]] const std::vector<RaceStatus>&                          getRaceStatus()                 const noexcept;
-        
-        [[nodiscard]] const std::vector<bool>&                                getRaceFinished()               const noexcept;
-        [[nodiscard]] const std::vector<bool>&                                getReleased()                   const noexcept;
-        
-        [[nodiscard]] const std::vector<std::pair<std::string, int>>&         getNearestNeighbors()           const noexcept;
 
 
         void setMasses(std::vector<FLOATING_NUMBER>&& massesKg);
         void setReactionTimes(std::vector<FLOATING_NUMBER>&& reactionTimes);
         void setDesiredSpeeds(std::vector<FLOATING_NUMBER>&& desiredSpeeds);
-        void setPropulsionForces(DimVector&& propulsionForces);
-        void setPropulsionForce(Dimensions&& propulsionForce, size_t index);
-        void setRepulsionForces(std::vector<FLOATING_NUMBER>&& repulsionForces);
-        void setNearestNeighbors(std::vector<std::pair<std::string, int>>&& nearestNeighbors);
-        void setPriorities(std::vector<FLOATING_NUMBER>&& priorities);
-        void setStartingAisles(std::vector<int>&& startingAisles);
         void setShoulderLengths(std::vector<FLOATING_NUMBER>&& shoulderLengths);
-        void setRaceStatus(RaceStatus status, size_t pedestrianIndex);
-        void setRaceCounter(int newCounter, size_t pedestrianIndex);
-        void setOpponentID(int newID, size_t pedestrianIndex);
-        void setRacePosition(Dimensions coordinates, size_t pedestrianIndex);
-        void setRaceFinished(bool isFinished, size_t newID);
-        void setReleased(bool hasReleased, size_t pedestrianIndex);
+
 
  private:
     int numPedestrians;
@@ -92,10 +62,6 @@ class CalmPedestrianSet : public PedestrianSet {
     DimVector                   pedestrianCoordinates;
     DimVector                   goalCoordinates;
     DimVector                   velocities;
-    DimVector                   racePositions;
-    DimVector                   propulsionForces;
-
-    std::vector<std::pair<std::string, int>>  nearestNeighbors;
 
 
 
@@ -103,18 +69,10 @@ class CalmPedestrianSet : public PedestrianSet {
     std::vector<FLOATING_NUMBER>              massesKg;
     std::vector<FLOATING_NUMBER>              reactionTimes;
     std::vector<FLOATING_NUMBER>              desiredSpeeds;
-    std::vector<FLOATING_NUMBER>              priorities;
     std::vector<FLOATING_NUMBER>              shoulderLengths;
 
-    std::vector<RaceStatus>                   raceStatus;
 
     std::vector<int>                          ids;
-    std::vector<int>                          raceCounter;
-    std::vector<int>                          opponentIDs;
-    std::vector<int>                          startingAisles;
-
-    std::vector<bool>                         raceFinished;
-    std::vector<bool>                         released;
 
     FLOATING_NUMBER startMass;
     FLOATING_NUMBER startReaction_time;
--- VIPRA/__tests__/Data_Sets/Pedestrian_Sets/tests.hpp
+++ VIPRA/__tests__/Data_Sets/Pedestrian_Sets/tests.hpp
@@ -1,1 +1,5 @@
 #pragma once
+
+#include "calm_pedestrian_set.tests.hpp"
+
+#define CALM_PEDESTRIAN_SET_TESTS calm_pedestrian_set_tests
