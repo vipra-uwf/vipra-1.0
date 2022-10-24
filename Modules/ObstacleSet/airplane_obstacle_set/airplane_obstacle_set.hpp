fb98151dcdefe0c723631e65cce62c43c3d583db
Rolland Goodenough <rtg13@students.uwf.edu>
2022-10-24 15:11:21 -0500

    #72 fixing const correctness

--- Modules/ObstacleSet/airplane_obstacle_set/airplane_obstacle_set.hpp
+++ Modules/ObstacleSet/airplane_obstacle_set/airplane_obstacle_set.hpp
@@ -5,33 +5,30 @@
 
 class AirplaneObstacleSet : public ObstacleSet {
 
-    private:
-        DimVector obstacleCoordinates;
-        CONFIG_MAP configurationMap;
+ private:
+  DimVector  obstacleCoordinates;
+  CONFIG_MAP configurationMap;
 
-    public:
+ public:
+  void configure(const CONFIG_MAP& configMap) override;
 
-        void configure(const CONFIG_MAP& configMap) override;
+  void addObjects(const std::string& type, const DimVector& locations) override;
 
-        void addObjects(const std::string& type,  const DimVector& locations) override;
+  void setObstacleCoords(const DimVector& coordinates) override;
+  int  getNumObstacles() const noexcept override;
 
-        void setObstacleCoords(const DimVector& coordinates) override;
-        int  getNumObstacles() const noexcept override;
+  const DimVector& getObstacleCoordinates() const noexcept override;
 
-        const DimVector& getObstacleCoordinates() const noexcept override;
+  const DimVector& getObjectsofType(const std::string& type) const noexcept override;
 
-        const DimVector& getObjectsofType(const std::string& type) const noexcept override;
+  const std::vector<std::string>& getObjectTypes() const noexcept override;
 
-        const std::vector<std::string>& getObjectTypes() const noexcept override;
+  void             initialize(const ENTITY_SET& MapType) noexcept override;
+  const DimVector  NearestObstacle(const PedestrianSet& PedSet) const override;
+  const Dimensions NearestObstacle(const Dimensions coordinates, const Dimensions velocity) const override;
 
-        void initialize(const ENTITY_SET& MapType) const noexcept override;
-        const DimVector& NearestObstacle(PedestrianSet PedSet) const noexcept override;
-        const DimVector& NearestObstacle(const Dimensions coordinates, const Dimensions velocity) const noexcept override;
-
-        const Dimensions getDimensions() const noexcept override;
-        const FLOATING_NUMBER rayHit(Dimensions, Dimensions) const noexcept override; 
-
- 
+  const Dimensions      getDimensions() const noexcept override;
+  const FLOATING_NUMBER rayHit(Dimensions, Dimensions) const noexcept override;
 };
 
 #endif
