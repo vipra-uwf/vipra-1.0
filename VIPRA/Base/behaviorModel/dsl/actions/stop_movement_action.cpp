c123482deb3a382c96a22168895d01ac00add738
Satkkeerthi Sriram <ss376@students.uwf.edu>
2022-12-14 13:19:47 -0600

    Error fixes in actions

--- VIPRA/Base/behaviorModel/dsl/actions/stop_movement_action.cpp
+++ VIPRA/Base/behaviorModel/dsl/actions/stop_movement_action.cpp
@@ -8,12 +8,11 @@ StopMovementAction::performAction(int                  pedestrianIndex,
                                   const PedestrianSet& pedestrianSet,
                                   const ObstacleSet&   obstacleSet,
                                   const Goals&         goals) {
-  static const VIPRA::f3d STOPPED = VIPRA::f3d{std::vector<float>{0, 0, 0}};
+  static const VIPRA::f3d STOPPED = VIPRA::f3d(0,0,0);
 
   if (!actionApplied(pedestrianIndex)) {
-    auto pedestrianSetObj = dynamic_cast<PedestrianSet*>(pedestrianSet);
-    const_cast<VIPRA::f3dVec&>(pedestrianSetObj->getVelocities()).at(pedestrianIndex) = STOPPED;
-    const_cast<VIPRA::f3d&>(pedestrianSetObj->getSpeeds()).at(pedestrianIndex) = 0;
-    const_cast<VIPRA::f3dVec&>(pedestrianSetObj->getPropulsionForces()).at(pedestrianIndex) = STOPPED;
+    const_cast<VIPRA::f3dVec&>(pedestrianSet.getVelocities()).at(pedestrianIndex) = STOPPED;
+    const_cast<std::vector<float>&>(pedestrianSet.getSpeeds()).at(pedestrianIndex) = 0;
+    const_cast<VIPRA::f3dVec&>(pedestrianSet.getPropulsionForces()).at(pedestrianIndex) = STOPPED;
   }
 }
