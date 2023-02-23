649f7d969ec5ba0e3d4f94a6f878b066fcfd4ea2
Nathan Rayon <nwr1@students.uwf.edu>
2023-02-22 19:37:02 -0600

    Adding config writer

---
+++ Modules/PedestrianLoader/calm_config_writer/calm_config_writer.mm
@@ -0,0 +1,8 @@
+{
+  "id"          : "ud89wadhaujdADHAhjdksa",
+  "name"        : "calm_config_writer",
+  "description" : "TODO",
+  "params"      : [],
+  "className"   : "CalmPedestrianConfigWriter",
+  "type"        : "configuration_writer"
+}
---
+++ VIPRA/Extendable/config_writer/config_writer.hpp
@@ -0,0 +1,30 @@
+#ifndef PEDESTRIAN_CONFIG_WRITER_HPP
+#define PEDESTRIAN_CONFIG_WRITER_HPP
+
+#include "definitions/config_map.hpp"
+#include "definitions/type_definitions.hpp"
+#include <memory>
+
+namespace VIPRA {
+  struct PedestrianBuilderData {
+    PedestrianBuilderData() {}
+    virtual ~PedestrianBuilderData() = default;
+  };
+}
+
+class PedestrianConfigWriterException : public std::runtime_error {
+ public:
+  PedestrianConfigWriterException(const std::string& message) : std::runtime_error(message) {}
+  static void Throw(const std::string& message) { throw PedestrianConfigWriterException(message); }
+};
+
+class PedestrianConfigWriter {
+    public:
+
+    virtual void configure(const VIPRA::Config::Map& configMap) = 0;
+    virtual void initialize() = 0;
+    virtual std::string buildPedestrians(const std::string& filePath) = 0;
+};
+
+
+#endif
